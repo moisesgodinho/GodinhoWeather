c9986156ec3463fc1bb81f96d8fd9a61a6d58d38
Moises Godinho <56728202+moisesgodinho@users.noreply.github.com>
2024-05-31 20:34:12 +0000

    add HourlyForecast module

---
+++ src/modules/HourlyForecast/index.jsx
@@ -0,0 +1,56 @@
+import styles from "./HourlyForecast.module.css";
+
+const {
+  hourlyForecast,
+  hourly,
+  tittle,
+  hourlyContent,
+  tittleHigh,
+  imgLi,
+  textoLi,
+  listItem,
+  ulHourly,
+} = styles;
+
+const HourlyForecast = ({ title, data }) => {
+  return (
+    <section className={hourlyForecast} id="hourlyForecast">
+      <p className={tittle}>{title}</p>
+
+      <ul className={ulHourly}>
+        {data.map((d, index) => (
+          <li key={index} className={listItem}>
+            <div className={hourly}>
+              <div className={hourlyContent}>
+                <p className={tittleHigh}>{d.title}</p>
+                <img className={imgLi} src={`${d.icon}`} alt="weather icon" />
+                <p className={textoLi}>{`${d.temp}°C`}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <ul className={ulHourly}>
+        {data.map((d, index) => (
+          <li key={index} className={listItem}>
+            <div className={hourly}>
+              <div className={hourlyContent}>
+                <p className={tittleHigh}>{d.title}</p>
+                <img
+                  className={imgLi}
+                  style={{ transform: `rotate(${d.deg}deg)` }}
+                  src={`src/assets/icons/direction.png`}
+                  alt="wind direction"
+                />
+                <p className={textoLi}>{`${d.speed}km/h`}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default HourlyForecast;
