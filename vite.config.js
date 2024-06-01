1873258bd47f4e68f9f191e896941ed4b153e44a
Moises Godinho <56728202+moisesgodinho@users.noreply.github.com>
2024-06-01 22:15:18 +0000

    add static icons

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       targets: [
         {
           src: 'src/assets/icons/*',
-          dest: 'assets/'
+          dest: 'src/assets/icons'
         }
       ]
     })
